f149ee9405aae4328190001564ee9f4c444ecb14
dsanderson90 <dsanderson3@cnm.edu>
2019-08-15 13:16:44 -0600

    first commit

---
+++ db.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID
+const dbname = 'fcc-node-crud';
+const url = 'mongodb+srv://admin:abcd1234@cluster0-myg00.mongodb.net/todos?retryWrites=true&w=majority'
+const mongoOptions = { useNewUrlParser: true};
+
+const state = {
+  db : null
+}
+
+const connect = (cb) => {
+  if(state.db)
+    cb();
+  else {
+    MongoClient.connect(url, mongoOptions, (err, client) => {
+      if(err)
+        cb(err);
+      else {
+        state.db = client.db(dbname);
+        cb();
+      }
+    })
+  }
+}
+
+const getPrimaryKey = (_id) => {
+  return ObjectID(_id);
+}
+
+const getDB = () => {
+  return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey}
