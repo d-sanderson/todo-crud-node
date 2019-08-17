6fc871fc4be52b453b8c987bd7f41ea6a7570f0c
dsanderson90 <dsanderson3@cnm.edu>
2019-08-16 19:38:17 -0600

    can now delete todos

--- models/todo.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-let TodoSchema = new Schema({
-  todo: {type: String, required: true, max: 400}
-})
-
-module.exports = mongoose.model('Todo', TodoSchema)
